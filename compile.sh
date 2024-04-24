b24f43af135312b65784f508773d175e9757c494
Dalibor Drevojanek <drevojanekdalibor@gmail.com>
2024-04-24 20:50:33 +0200

    add build dirOC

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 mkdir -p build
 gcc src/main.c -o build/main.o -c
 gcc src/ini_parser.c -o build/ini_parser.o -c
-gcc -o buid/test build/main.o build/ini_parser.o
+gcc -o build/test build/main.o build/ini_parser.o
+cp src/test.ini build/
